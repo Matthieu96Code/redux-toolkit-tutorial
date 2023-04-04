5bca6ebe4effeb57dfb278d7d92a8b73c9e9ee26
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-04 02:57:35 +0000

    Implement calculate total

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -30,7 +30,7 @@ const CartContainer = () => {
           <hr />
           <div className='cart-total'>
             <h4>
-              total <span>${total}</span>
+              total <span>${total.toFixed(2)}</span>
             </h4>
           </div>
           <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>clear cart</button>
